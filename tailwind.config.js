b8af530e05a6c368a91121d1d105c1e95afd8294
ohidson <ohidson@gmail.com>
2024-12-31 21:58:48 -0900

    updating files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// In tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['"Playfair Display"', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'lobster': ['Lobster', 'cursive'],
+        // Add more fonts as needed
+      }
+    }
+  }
+}
